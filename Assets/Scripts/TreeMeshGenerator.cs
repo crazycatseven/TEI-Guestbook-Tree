b112eb480503d0ee15642708a2b5ea46ed7806e6
crzaycatseven <80610581+crzaycatseven@users.noreply.github.com>
2023-04-24 04:48:04 +1000

    Add trunk mesh

---
+++ Assets/Scripts/TreeMeshGenerator.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TreeMeshGenerator
+{
+    public static Mesh CreateTrunkMesh(Vector3[] trunkVertices, float trunkRadius, int radialSegments)
+    {
+        // 方法实现
+
+        // Step 1: Create a new Mesh instance
+        Mesh trunkMesh = new Mesh();
+
+        // Step 2: Create a ring for each vertex and store them in a vertex array
+        int vertexCount = trunkVertices.Length * (radialSegments + 1);
+        Vector3[] vertices = new Vector3[vertexCount];
+        Vector2[] uv = new Vector2[vertexCount];
+
+        for (int i = 0; i < trunkVertices.Length; i++)
+        {
+            for (int j = 0; j <= radialSegments; j++)
+            {
+                float angle = 2 * Mathf.PI * j / radialSegments;
+                float x = trunkRadius * Mathf.Cos(angle);
+                float z = trunkRadius * Mathf.Sin(angle);
+
+                Vector3 vertexOffset = new Vector3(x, 0, z);
+                vertices[i * (radialSegments + 1) + j] = trunkVertices[i] + vertexOffset;
+                uv[i * (radialSegments + 1) + j] = new Vector2((float)j / radialSegments, (float)i / (trunkVertices.Length - 1));
+            }
+        }
+
+        // Step 3: Create triangles based on the vertex array and store them in a triangle array
+        int[] triangles = new int[(trunkVertices.Length - 1) * radialSegments * 6];
+        for (int i = 0; i < trunkVertices.Length - 1; i++)
+        {
+            for (int j = 0; j < radialSegments; j++)
+            {
+                int baseIndex = i * (radialSegments + 1) + j;
+                int triangleIndex = (i * radialSegments + j) * 6;
+
+                triangles[triangleIndex] = baseIndex;
+                triangles[triangleIndex + 1] = baseIndex + radialSegments + 1;
+                triangles[triangleIndex + 2] = baseIndex + 1;
+                triangles[triangleIndex + 3] = baseIndex + 1;
+                triangles[triangleIndex + 4] = baseIndex + radialSegments + 1;
+                triangles[triangleIndex + 5] = baseIndex + radialSegments + 2;
+            }
+        }
+
+        // Step 4: Assign the vertex array and triangle array to the Mesh
+        trunkMesh.vertices = vertices;
+        trunkMesh.triangles = triangles;
+        trunkMesh.uv = uv;
+        trunkMesh.RecalculateNormals();
+
+        // Step 5: Return the Mesh
+        return trunkMesh;
+        //
+    }
+}
